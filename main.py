ae28d97f32bc6b70df2636b96f0f769945146e8d
Elle09 <elle.adelman@gmail.com>
2025-06-06 00:40:46 -0400

    Task 2

--- main.py
+++ main.py
@@ -107,7 +107,64 @@ def load_transactions(filename="financial_transactions.csv"):
         print(tx)
 
 
+def add_transaction(transactions):
 
+    try:
+
+        date_str = input("Enter date (YYY-MM-DD): ")
+        date = datetime.strptime(date_str, "%Y-%m-%d").date()
+
+        customer_id = int(input("Enter customer ID: "))
+
+        amount = float(input("Enter transaction amount: "))
+
+        tx_type = input("Enter transaction type (credit/debit/transfer: ").lower() # .lower() removes case sensitivity in check 
+        if tx_type not in [
+            "credit",
+            "debit",
+            "transfer" 
+        ]:
+
+            print(
+                "Invalid transaction type. Must be either credit, debit, or transfer."
+            )
+            return
+
+        description = input("Enter transaction description: ")
+
+        if transactions:
+            transaction_id = max(tx["transaction_id"] for tx in transactions) + 1
+        else:
+            transaction_id = 1
+
+        if tx_type == "debit":
+            amount = -amount
+
+        new_tx = {
+            "transaction_id": transaction_id,
+            "date": date,
+            "customer_id": customer_id,
+            "amount": amount,
+            "type": tx_type,
+            "description": description,
+        }
+
+        transactions.append(new_tx) # WHY ARE YOU WHITE UGH
+        print("Transaction added!")
+
+    except ValueError as e:
+        print(f"Input error: {e}")
+
+
+def view_transactions(transactions):
+    print(f"{'ID':<4} | {'Date':<12} | {'Customer':<8} | {'Amount':<10} | {'Type':<8} | Description") # fixed nested quotes??
+    print("-" * 75)
+
+    for tx in transactions: 
+        date_str = tx["date"].strftime("%b %d, %Y")
+        amount_str = f"{tx['amount']:.2f}"
+
+        print(f"{tx['transaction_id']:<4} | {date_str:<12} | {tx['customer_id']:<8} | {amount_str:<10} | {tx['type']:<8} | {tx['description']}")
 
 
 
